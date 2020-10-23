537e4bda1bbc403c2574444ff475433377dbf1f6
Agustin <buddy.m091@gmail.com>
2020-10-23 16:59:05 -0400

    Add maps

--- src/UI/CardCustomerRounded.js
+++ src/UI/CardCustomerRounded.js
@@ -7,11 +7,10 @@ import {
     Image
 } from 'react-native';
 
-export default function CardRace(props) {
+export default function CardCustomerRounded(props) {
     return (
-        <TouchableOpacity onPress={props.onPress} >
             <View style={ [styles.cardContainer] }>
-                <View style={{ width: '20%', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <View style={{ width: '20%', flex: 1 }}>
                     <Image
                         style={{ width: 50, height: 50, borderRadius: 25 }}
                         source={{uri: 'https://reactnative.dev/img/tiny_logo.png',}}
@@ -19,13 +18,12 @@ export default function CardRace(props) {
                 </View>
                 <View style={{ width: '60%' }}>
                     <Text style={{ fontSize: 18, color: 'black' }} numberOfLines={1}>{ props.customer }</Text>
-                    <Text style={{ fontSize: 13, color: '#909090', marginTop: 3 }} numberOfLines={2}>{ props.description }</Text>
+                    <Text style={{ fontSize: 13, color: '#909090', marginTop: 3 }} numberOfLines={1}>{ props.description }</Text>
                 </View>
                 <View style={{ width: '20%', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                     <Text style={{ fontSize: 20, color: '#5B5C5E' }}>{ props.amount }</Text>
                 </View>
             </View>
-        </TouchableOpacity>
     );
 }
 
@@ -37,14 +35,13 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         shadowColor: '#ddd',
         shadowOffset: { width: 0, height: 3 },
-        borderRadius: 5,
+        borderRadius: 25,
         shadowOpacity: 0.8,
         shadowRadius: 2,
-        height: 80,
+        height: 50,
         width: '100%',
         elevation: 3,
         borderColor: '#ddd',
-        padding: 10,
-        marginBottom: 2
+        marginTop: 20
     }
 });
